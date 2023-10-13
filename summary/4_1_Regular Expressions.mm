a2f91d3de6cfd9e48c41c279ce6088a947fab176
farisubuntu <farisubuntu@gmail.com>
2023-10-13 18:38:15 +0300

    add bash_resources_page

--- summary/4_1_Regular Expressions.mm
+++ summary/4_1_Regular Expressions.mm
@@ -1,6 +1,6 @@
 <map version="freeplane 1.7.0">
 <!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
-<node TEXT="4. Regular Expressions" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_493211461" CREATED="1697084874571" MODIFIED="1697145519873" MAX_WIDTH="250.0 pt" MIN_WIDTH="120.0 pt" VGAP_QUANTITY="0.0 pt"><hook NAME="MapStyle" zoom="1.467">
+<node TEXT="4. Regular Expressions" LOCALIZED_STYLE_REF="AutomaticLayout.level.root" FOLDED="false" ID="ID_493211461" CREATED="1697084874571" MODIFIED="1697191667330" MAX_WIDTH="250.0 pt" MIN_WIDTH="120.0 pt" VGAP_QUANTITY="0.0 pt"><hook NAME="MapStyle" zoom="1.213">
     <properties edgeColorConfiguration="#808080ff,#000000ff,#ff0033ff,#009933ff,#3333ffff,#ff6600ff,#cc00ccff,#ffbf00ff,#00ff99ff,#0099ffff,#996600ff,#000000ff,#cc0066ff,#33ff00ff,#ff9999ff,#0000ccff,#cccc00ff,#0099ccff,#006600ff,#ff00ccff,#00cc00ff,#0066ccff,#00ffffff" show_icon_for_attributes="true" show_note_icons="true" fit_to_viewport="false"/>
 
 <map_styles>
@@ -9,8 +9,8 @@
 <richcontent TYPE="DETAILS" LOCALIZED_HTML="styles_background_html"/>
 <stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right" STYLE="bubble">
 <font SIZE="9"/>
-<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="bubble" SHAPE_VERTICAL_MARGIN="0.0 pt" TEXT_ALIGN="CENTER" MAX_WIDTH="120.0 pt" MIN_WIDTH="120.0 pt">
-<font NAME="Arial" SIZE="9" BOLD="true" ITALIC="false"/>
+<stylenode LOCALIZED_TEXT="default" ICON_SIZE="12.0 pt" COLOR="#000000" STYLE="bubble" SHAPE_VERTICAL_MARGIN="0.0 pt" TEXT_ALIGN="LEFT" MAX_WIDTH="450.0 pt" MIN_WIDTH="120.0 pt">
+<font NAME="Arial" SIZE="12" BOLD="true" ITALIC="false"/>
 <edge STYLE="horizontal" WIDTH="3"/>
 </stylenode>
 <stylenode LOCALIZED_TEXT="defaultstyle.details" COLOR="#99ff99" BACKGROUND_COLOR="#3333ff" STYLE="fork" SHAPE_VERTICAL_MARGIN="0.0 pt" FORMAT="NO_FORMAT">
@@ -53,15 +53,14 @@
 </map_styles>
 </hook>
 <hook NAME="AutomaticEdgeColor" COUNTER="0" RULE="FOR_COLUMNS"/>
-<node TEXT="- A regular expression is a pattern that describes a set of strings.&#xa;- A regular expression may be followed by one of several repetition operators (metacharacters)" LOCALIZED_STYLE_REF="defaultstyle.details" POSITION="left" ID="ID_62451043" CREATED="1697090865263" MODIFIED="1697120880730" HGAP_QUANTITY="-255.99999195337324 pt" VSHIFT_QUANTITY="-266.2499920651319 pt" COLOR="#ffff33" BACKGROUND_COLOR="#3333ff" TEXT_ALIGN="LEFT" MAX_WIDTH="400.0 pt">
+<hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="HEADINGS"/>
+<node TEXT="- A regular expression is a pattern that describes a set of strings.&#xa;- A regular expression may be followed by one of several repetition operators (metacharacters)" LOCALIZED_STYLE_REF="defaultstyle.details" POSITION="left" ID="ID_62451043" CREATED="1697090865263" MODIFIED="1697191653725" HGAP_QUANTITY="-37.7499984577299 pt" VSHIFT_QUANTITY="-368.9999890029434 pt" COLOR="#ffff33" BACKGROUND_COLOR="#3333ff" TEXT_ALIGN="LEFT" MAX_WIDTH="400.0 pt">
 <font NAME="DejaVu Serif" SIZE="14" BOLD="true"/>
 </node>
-<node TEXT="NOTES" LOCALIZED_STYLE_REF="defaultstyle.note" POSITION="right" ID="ID_1720603091" CREATED="1697090926271" MODIFIED="1697147534427" VGAP_QUANTITY="0.0 pt" TEXT_ALIGN="CENTER">
+<node TEXT="NOTES" LOCALIZED_STYLE_REF="defaultstyle.note" POSITION="right" ID="ID_1720603091" CREATED="1697090926271" MODIFIED="1697202496234" VGAP_QUANTITY="0.0 pt" TEXT_ALIGN="CENTER">
 <font SIZE="16"/>
-<node TEXT="Two regular expressions may be concatenated; the resulting regular expression matches any string formed by concatenating two substrings that respectively match the concatenated subexpressions." LOCALIZED_STYLE_REF="defaultstyle.note" ID="ID_279204513" CREATED="1697090769390" MODIFIED="1697127596049" HGAP_QUANTITY="31.99999946355821 pt" VSHIFT_QUANTITY="17.249999485909946 pt" BACKGROUND_COLOR="#660000" TEXT_ALIGN="LEFT" MAX_WIDTH="450.0 pt">
-<font SIZE="14" ITALIC="false"/>
-</node>
-<node LOCALIZED_STYLE_REF="defaultstyle.note" ID="ID_761600431" CREATED="1697090769390" MODIFIED="1697127596051" VGAP_QUANTITY="2.0 pt" HGAP_QUANTITY="32.74999944120647 pt" VSHIFT_QUANTITY="2.9999999105930604 pt" MAX_WIDTH="450.0 pt" MIN_WIDTH="120.0 pt" BACKGROUND_COLOR="#660000"><richcontent TYPE="NODE">
+<node TEXT="Two regular expressions may be concatenated; the resulting regular expression matches any string formed by concatenating two substrings that respectively match the concatenated subexpressions." LOCALIZED_STYLE_REF="default" ID="ID_279204513" CREATED="1697090769390" MODIFIED="1697191472359" HGAP_QUANTITY="31.99999946355821 pt" VSHIFT_QUANTITY="17.249999485909946 pt"/>
+<node LOCALIZED_STYLE_REF="default" ID="ID_761600431" CREATED="1697090769390" MODIFIED="1697191472355" HGAP_QUANTITY="32.74999944120647 pt" VSHIFT_QUANTITY="2.9999999105930604 pt"><richcontent TYPE="NODE">
 
 <html>
   <head>
@@ -74,9 +73,8 @@
   </body>
 </html>
 </richcontent>
-<font SIZE="14"/>
 </node>
-<node LOCALIZED_STYLE_REF="styles.important" ID="ID_1740808549" CREATED="1697091434884" MODIFIED="1697128080515" COLOR="#cccc00" BACKGROUND_COLOR="#660000" FORMAT="NO_FORMAT" TEXT_ALIGN="LEFT" MAX_WIDTH="450.0 pt" HGAP_QUANTITY="31.999999463558204 pt" VSHIFT_QUANTITY="2.6645352591003757E-15 pt"><richcontent TYPE="NODE">
+<node LOCALIZED_STYLE_REF="default" ID="ID_1740808549" CREATED="1697091434884" MODIFIED="1697202877589" HGAP_QUANTITY="31.999999463558204 pt" VSHIFT_QUANTITY="2.6645352591003757E-15 pt"><richcontent TYPE="NODE">
 
 <html>
   <head>
@@ -89,9 +87,8 @@
   </body>
 </html>
 </richcontent>
-<edge STYLE="sharp_bezier"/>
 </node>
-<node LOCALIZED_STYLE_REF="defaultstyle.note" ID="ID_769229836" CREATED="1697120701849" MODIFIED="1697147534426" BACKGROUND_COLOR="#660000" MAX_WIDTH="450.0 pt" HGAP_QUANTITY="33.49999941885471 pt" VSHIFT_QUANTITY="-2.9999999105930355 pt"><richcontent TYPE="NODE">
+<node LOCALIZED_STYLE_REF="default" ID="ID_769229836" CREATED="1697120701849" MODIFIED="1697202875366" HGAP_QUANTITY="29.74999953061342 pt" VSHIFT_QUANTITY="-14.24999957531692 pt"><richcontent TYPE="NODE">
 
 <html>
   <head>
@@ -99,32 +96,143 @@
   </head>
   <body>
     <p>
-      In basic regular expressions the <i>metacharacters</i>&#160;&quot;?&quot;, &quot;+&quot;, &quot;{&quot;, &quot;|&quot;, &quot;(&quot;, and &quot;)&quot; lose their special meaning; instead use the backslashed versions &quot;\?&quot;, &quot;\+&quot;, &quot;\{&quot;, &quot;\|&quot;, &quot;\(&quot;, and &quot;\)&quot;.
+      - In basic regular expressions the <i>metacharacters</i>&#160;&quot;?&quot;, &quot;+&quot;, &quot;{&quot;, &quot;|&quot;, &quot;(&quot;, and &quot;)&quot; lose their special meaning; instead use the backslashed versions &quot;\?&quot;, &quot;\+&quot;, &quot;\{&quot;, &quot;\|&quot;, &quot;\(&quot;, and &quot;\)&quot;.
+    </p>
+    <p>
+      - Encapsulating the variable name with ${} is used to avoid ambiguity(&#1594;&#1605;&#1608;&#1590;/&#1573;&#1604;&#1578;&#1576;&#1575;&#1587;):
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      <font color="#ff0000">&#160;PRICE=160 </font>
+    </p>
+    <p>
+      <font color="#ff0000">&#160;echo &quot;The total by \$ dollar is : $PRICE&quot; </font>
+    </p>
+    <p>
+      <font color="#ff0000">&#160;MyFirstLetters=&quot;Ebrah&quot; </font>
+    </p>
+    <p>
+      <font color="#ff0000">&#160;echo &quot;my name is ${MyFirstLetters}eem&quot;</font>
     </p>
   </body>
 </html>
+
 </richcontent>
-<font SIZE="14"/>
 </node>
-<node TEXT="Check wether commands using regular expressions support extended expressions.." LOCALIZED_STYLE_REF="defaultstyle.note" ID="ID_1769657325" CREATED="1697123762458" MODIFIED="1697128092499" BACKGROUND_COLOR="#660000" MAX_WIDTH="450.0 pt" HGAP_QUANTITY="29.749999530613433 pt" VSHIFT_QUANTITY="-80.99999758601196 pt">
-<font SIZE="14"/>
+<node TEXT="" ID="ID_506141328" CREATED="1697202864323" MODIFIED="1697202864323"/>
+<node TEXT="Check wether commands using regular expressions support extended expressions.." LOCALIZED_STYLE_REF="default" ID="ID_1769657325" CREATED="1697123762458" MODIFIED="1697202496233" HGAP_QUANTITY="28.999999552965175 pt" VSHIFT_QUANTITY="-60.74999818950897 pt"/>
 </node>
-</node>
-<node TEXT="Metacharacters" LOCALIZED_STYLE_REF="defaultstyle.note" POSITION="right" ID="ID_31172513" CREATED="1697120828665" MODIFIED="1697123742424" TEXT_ALIGN="CENTER" VGAP_QUANTITY="0.0 pt">
+<node TEXT="Metacharacters" LOCALIZED_STYLE_REF="defaultstyle.note" POSITION="right" ID="ID_31172513" CREATED="1697120828665" MODIFIED="1697202499057" TEXT_ALIGN="CENTER" VGAP_QUANTITY="0.0 pt">
 <font SIZE="16"/>
-<node TEXT="Regular expression operators" ID="ID_1663621727" CREATED="1697090109147" MODIFIED="1697123742424" HGAP_QUANTITY="17.749999888241295 pt" VSHIFT_QUANTITY="57.74999827891593 pt">
+<node TEXT="Regular expression operators" ID="ID_1663621727" CREATED="1697090109147" MODIFIED="1697202499057" HGAP_QUANTITY="15.499999955296518 pt" VSHIFT_QUANTITY="57.74999827891593 pt">
 <hook URI="../../../../Pictures/table4-1_Regular_expression_operators.png" SIZE="0.4691165" NAME="ExternalObject"/>
 </node>
 </node>
-<node TEXT="Basic Commands" LOCALIZED_STYLE_REF="defaultstyle.note" POSITION="left" ID="ID_1831091726" CREATED="1697145412206" MODIFIED="1697146157520" TEXT_ALIGN="CENTER" VGAP_QUANTITY="0.0 pt" HGAP_QUANTITY="25.999999642372188 pt" VSHIFT_QUANTITY="59.249998234212455 pt">
+<node TEXT="Basic Commands" LOCALIZED_STYLE_REF="defaultstyle.note" FOLDED="true" POSITION="left" ID="ID_1831091726" CREATED="1697145412206" MODIFIED="1697191645920" TEXT_ALIGN="CENTER" VGAP_QUANTITY="0.0 pt" HGAP_QUANTITY="164.74999550730007 pt" VSHIFT_QUANTITY="-36.74999890476468 pt">
 <font SIZE="16"/>
 <node TEXT="cat" LOCALIZED_STYLE_REF="defaultstyle.details" ID="ID_95973369" CREATED="1697145546248" MODIFIED="1697146138127" COLOR="#ffffff" BACKGROUND_COLOR="#000099" STYLE="bubble" SHAPE_VERTICAL_MARGIN="1.8 pt" FORMAT="NO_FORMAT" MAX_WIDTH="450.0 pt" VGAP_QUANTITY="0.0 pt" HGAP_QUANTITY="61.99999856948858 pt" VSHIFT_QUANTITY="73.49999780952938 pt">
 <font NAME="Monospaced" SIZE="16"/>
 <hook URI="../../../../Pictures/Screenshot_2023-10-13_00-23-29.png" SIZE="1.0" NAME="ExternalObject"/>
 </node>
-<node TEXT="grep: Search for PATTERNS in each FILE&#xa;Usage: grep [OPTION]... PATTERNS [FILE]..." LOCALIZED_STYLE_REF="defaultstyle.details" ID="ID_1401237480" CREATED="1697125344262" MODIFIED="1697146157519" VGAP_QUANTITY="0.0 pt" HGAP_QUANTITY="61.24999859184031 pt" VSHIFT_QUANTITY="13.499999597668666 pt" COLOR="#ffffff" BACKGROUND_COLOR="#000099" STYLE="bubble" SHAPE_VERTICAL_MARGIN="1.8 pt" FORMAT="NO_FORMAT" MAX_WIDTH="450.0 pt" MIN_WIDTH="120.0 pt" BORDER_WIDTH_LIKE_EDGE="true">
-<hook URI="../../../../Pictures/Screenshot_2023-10-12_18-42-46.png" SIZE="1.0" NAME="ExternalObject"/>
+<node TEXT="grep: Search for PATTERNS in each FILE&#xa;Usage: grep [OPTION]... PATTERNS [FILE]..." LOCALIZED_STYLE_REF="defaultstyle.details" ID="ID_1401237480" CREATED="1697125344262" MODIFIED="1697191500816" VGAP_QUANTITY="0.0 pt" HGAP_QUANTITY="29.749999530613437 pt" VSHIFT_QUANTITY="93.74999720603236 pt" COLOR="#ffffff" BACKGROUND_COLOR="#000099" STYLE="bubble" SHAPE_VERTICAL_MARGIN="1.8 pt" FORMAT="NO_FORMAT" MAX_WIDTH="450.0 pt" MIN_WIDTH="120.0 pt" BORDER_WIDTH_LIKE_EDGE="true">
 <font NAME="Monospaced" SIZE="16"/>
+<hook URI="../../../../Pictures/Screenshot_2023-10-12_18-42-46.png" SIZE="1.0" NAME="ExternalObject"/>
+</node>
+</node>
+<node TEXT="Character classes" LOCALIZED_STYLE_REF="defaultstyle.note" POSITION="right" ID="ID_1265498915" CREATED="1697159666022" MODIFIED="1697191085390" TEXT_ALIGN="CENTER" VGAP_QUANTITY="0.0 pt" HGAP_QUANTITY="9.500000134110447 pt" VSHIFT_QUANTITY="45.74999863654379 pt">
+<font SIZE="16"/>
+<node LOCALIZED_STYLE_REF="default" ID="ID_1802953052" CREATED="1697159904375" MODIFIED="1697197585039" HGAP_QUANTITY="3.5000003129243753 pt" VSHIFT_QUANTITY="1.4999999552965178 pt"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      A bracket expression is a list of characters enclosed by &quot;<font color="#ff0000">[</font>&quot; and &quot;<font color="#ff0000">]</font>&quot;. It matches any single character in that list; if the first character of the list is the caret, &quot;<font color="#ff0000">^</font>&quot;, then it matches any character NOT in the list. For example, the regular expression &quot;[0123456789]&quot; matches any single digit.
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      Within a bracket expression, a range expression consists of two characters separated by a hyphen. It matches any single character that sorts between the two characters.
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node TEXT="Wildcards" LOCALIZED_STYLE_REF="defaultstyle.note" POSITION="right" ID="ID_1508321877" CREATED="1697160031639" MODIFIED="1697191667330" TEXT_ALIGN="CENTER" VGAP_QUANTITY="0.0 pt" HGAP_QUANTITY="31.99999946355817 pt" VSHIFT_QUANTITY="17.249999485909996 pt">
+<font SIZE="16"/>
+<node LOCALIZED_STYLE_REF="default" ID="ID_965431171" CREATED="1697191175158" MODIFIED="1697191575002"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      - Use the &quot;<font color="#ff0000">.</font>&quot; for a single character match.
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node ID="ID_345087942" CREATED="1697191175158" MODIFIED="1697191588622" TEXT_ALIGN="LEFT" MAX_WIDTH="450.0 pt"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      - Use the &quot;<font color="#ff0000">*</font>&quot; for multiple characters match.
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<font SIZE="12"/>
+</node>
+<node LOCALIZED_STYLE_REF="default" ID="ID_747631723" CREATED="1697191175161" MODIFIED="1697191564610"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      - To find the literal asterisk character in a file or output, use single quotes. for ex.<font color="#008000">&#160;</font><font color="#ff0000"><i><u>$</u></i><u>&#160;<i>grep '*' /etc/profile</i></u><i>&#160; </i></font>
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node LOCALIZED_STYLE_REF="default" ID="ID_1445383061" CREATED="1697191175162" MODIFIED="1697191552183"><richcontent TYPE="NODE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      For example, to get a list of all five-character English dictionary words starting with &quot;c&quot; and ending in &quot;h&quot; (handy for solving crosswords):
+    </p>
+    <p>
+      
+    </p>
+    <p>
+      <i><font color="#ff0000">$ grep '\&lt;c...h\&gt;' /usr/share/dict/words</font></i>
+    </p>
+  </body>
+</html>
+
+</richcontent>
 </node>
 </node>
 </node>
